c71a2de62c3cf5ec3b606a517692ef43bfad7d75
Evgenii Im <evgenii.im@jetbrains.com>
2025-04-24 17:21:17 +0200

    Test again

--- src/main/java/org/example/SomeClass.kt
+++ src/main/java/org/example/SomeClass.kt
@@ -1,7 +1,10 @@
 package org.example
 
+import com.sun.org.slf4j.internal.LoggerFactory
+
 class SomeClass {
     companion object {
+        private val logger = LoggerFactory.getLogger(SomeClass::class.java)
         const val I = 1
     }
 
