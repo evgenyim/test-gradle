1883db092db2fb4829a7866a3e8a3faa0daae5a3
Evgenii Im <evgenii.im@jetbrains.com>
2025-04-24 15:08:27 +0200

    Add obsolete string2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         String obsolete = "Obsolete";
+        String obsolete2 = "Obsolete2";
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
